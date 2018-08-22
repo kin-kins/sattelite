26a4435f34745dfa83144c843eb48b43fad1a704
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2018-08-22 21:49:38 +0530

    Add files via upload

---
+++ sattelite_current/sattelite_current.ino
@@ -0,0 +1,153 @@
+///÷Æ÷
+#include<Wire.h>
+#include <SFE_BMP180.h>
+#include <SD.h> //Load SD library
+int chipSelect = 4; //chip select pin for the MicroSD Card Adapter 
+int c=0;
+SFE_BMP180 pressure;
+File file; // file object that is used to read and write data
+#define ALTITUDE 1655.0 // Altitude of SparkFun's HQ in Boulder, CO. in meters
+const int MPU_addr=0x68;
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+void setup() {
+  Serial.begin(9600); // start serial connection to print out debug messages and data
+   Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  pinMode(chipSelect, OUTPUT); // chip select pin must be set to OUTPUT mode
+  if (!SD.begin(chipSelect)) { // Initialize SD card
+    Serial.println("Could not initialize SD card."); // if return value is false, something went wrong.
+  }
+  
+  if (SD.exists("file.txt")) { // if "file.txt" exists, fill will be deleted
+    Serial.println("File exists.");
+    if (SD.remove("file.txt") == true) {
+      Serial.println("Successfully removed file.");
+    } else {
+      Serial.println("Could not removed file.");
+    }
+  }
+   file = SD.open("file.txt", FILE_WRITE);
+  delay(1000);
+  Serial.println("REBOOT");
+  if (pressure.begin())
+    Serial.println("BMP180 init success");
+  else
+  {
+    Serial.println("BMP180 init fail\n\n");
+    while(1); // Pause forever.
+  }
+}
+void loop() {
+   Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+  delay(333);
+  // open "file.txt" to write data . BMP AND UV
+  char status;
+  double T,P,p0,a;
+  Serial.println();
+  Serial.print("provided altitude: ");
+  Serial.print(ALTITUDE,0);
+  Serial.print(" meters, ");
+  Serial.print(ALTITUDE*3.28084,0);
+  Serial.println(" feet");
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    delay(status);
+
+   
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      Serial.print("temperature: ");
+      Serial.print(T,2);
+      Serial.print(" deg C, ");
+      Serial.print((9.0/5.0)*T+32.0,2);
+      Serial.println(" deg F");
+     
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        delay(status);
+
+       
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          Serial.print("absolute pressure: ");
+          Serial.print(P,2);
+          Serial.print(" mb, ");
+          Serial.print(P*0.0295333727,2);
+          Serial.println(" inHg");
+
+        
+          p0 = pressure.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
+          Serial.print("relative (sea-level) pressure: ");
+          Serial.print(p0,2);
+          Serial.print(" mb, ");
+          Serial.print(p0*0.0295333727,2);
+          Serial.println(" inHg");
+
+         
+          a = pressure.altitude(P,p0);
+          Serial.print("computed altitude: ");
+          Serial.print(a,0);
+          Serial.print(" meters, ");
+          Serial.print(a*3.28084,0);
+          Serial.println(" feet");
+        }
+        else Serial.println("error retrieving pressure measurement\n");
+      }
+      else Serial.println("error starting pressure measurement\n");
+    }
+    else Serial.println("error retrieving temperature measurement\n");
+  }
+  else Serial.println("error starting temperature measurement\n");
+
+int uv=analogRead(A0);
+Serial.print("UV Radiation ");
+Serial.println(uv);
+delay(400);
+  delay(5000); 
+
+
+//CLOSE UV AND BMP
+  
+  if (file) {
+    file.print(AcX);file.print(",");file.print(AcY);file.print(",");file.print(AcZ);
+    file.print(GyX);file.print(",");file.print(GyY);file.print(",");file.print(GyZ);file.print(",");file.print(Tmp);
+    file.print(",");file.print(P);file.print(",");file.print(p0);file.print(",");file.print(a);
+    file.print(",");file.print(uv);
+    file.println(); // write number to file
+   // file.close(); // close file
+   
+    //file.close(); // close file
+    Serial.print("Wrote MPU constraints "); // debug output: show written number in serial monitor
+    
+  } else {
+    Serial.println("Could not open file (writing).");
+  }
+  if(c==10)
+  file.close();
+  c++;
+  delay(500); // wait for 5000ms
+}
